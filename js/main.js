37e7fdafc33df595d994343c93b3aeeff9f690e2
Alan Yeh <alan_yeh@apple.com>
2016-09-20 11:28:40 -0700

    countdown timer finally workingzzzz

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-// var timer = document.getElementById('timer');
-// var toggleBtn = document.getElementById('toggle');
-// var resetBtn = document.getElementById('reset');
-//
-// var watch = new Stopwatch(timer);
-//
-// toggleBtn.addEventListener('click', function() {
-// 	if (watch.isOn) {
-// 		watch.stop();
-// 	} else {
-// 		watch.start();
-// 	}
-// });
-//
-// resetBtn.addEventListener('click', function() {
-// 	watch.reset();
-// });
-//
-//
-// // console.log('hi');
+var timer = document.getElementById('timer');
+var toggleBtn = document.getElementById('toggle');
+var resetBtn = document.getElementById('reset');
+
+var watch = new Stopwatch(timer);
+
+toggleBtn.addEventListener('click', function() {
+	if (watch.isOn) {
+		watch.stop();
+		toggleBtn.textContent = 'Start';
+	} else {
+		watch.start();
+		toggleBtn.textContent = 'Stop';
+	}
+});
+
+resetBtn.addEventListener('click', function() {
+	watch.reset();
+});
+
+
+// console.log('hi');
--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
-function Stopwatch() {
+function Stopwatch(elem) {
 	var time = 0;
   var interval;
   var offset;
 
   function update() {
-  	time += delta(); // added to time for each update
+		if (this.isOn) {
+			time += delta(); // added to time for each update
+		}
 		var formattedTime = timeFormatter(time);
+		elem.textContent = formattedTime;
 		console.log(formattedTime);
   }
 
   function delta() {
   	var now = Date.now();
-    var timePassed = now - offset;
-    offset = now; // calculates the new now number from above
+		// console.log(now);
+    var timePassed = offset - now;
+    offset = now; // calculates the new now number from about
     return timePassed;
   }
 
   function timeFormatter(timeInMilliseconds) {
-		var time = new Date(timeInMilliseconds); // we can now get a bunch of date methods
+		var time = new Date((timeInMilliseconds)); // we can now get a bunch of date methods
+		// console.log(time);
 		var minutes = time.getMinutes().toString();
 		var seconds = time.getSeconds().toString();
 		var milliseconds = time.getMilliseconds().toString();
@@ -43,8 +48,8 @@ function Stopwatch() {
 
   this.start = function() {
   	if (!this.isOn) {
-	    interval = setInterval(update, 10); //updates every 10 milliseconds
-    	offset = Date.now();
+	    interval = setInterval(update.bind(this), 10); //updates every 10 milliseconds
+    	offset = new Date(Date.parse(new Date()) + 30 * 60 * 1000);
     	this.isOn = true;
     }
   };
@@ -57,6 +62,9 @@ function Stopwatch() {
     }
   };
 
-  this.reset = function() {};
-	time = 0;
+  this.reset = function() {
+		this.stop();
+		time = 0;
+		update();
+	};
 }
