cb8c875684b2a078228c479500c026377c6ff5b5
Alan Yeh <alan_yeh@apple.com>
2016-09-22 15:47:06 -0700

    first interval timer

--- js/script.js
+++ js/script.js
@@ -1,68 +1,27 @@
-function Stopwatch(elem) {
-	var time = 0;
-  var interval;
-  var offset = new Date(Date.parse(new Date()) + 30 * 60 * 1000);
+var timer = document.getElementById('timer');
+var toggleBtn = document.getElementById('toggle');
+var resetBtn = document.getElementById('reset');
+var plusBtn = document.getElementById('add');
 
-  function update() {
-		if (this.isOn) {
-			time += delta(); // added to time for each update
-		}
-		var formattedTime = timeFormatter(time);
-		elem.textContent = formattedTime;
-  }
-
-  function delta() {
-  	var now = Date.now();
-    var timePassed = offset - now;
-    offset = now; // calculates the new now number from about
-    return timePassed;
-  }
-
-  function timeFormatter() {
-		var timer = duration, minutes, seconds;
-		setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
+function startTimer(duration, display) {
+    var t = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(t / 60, 10)
+        seconds = parseInt(t % 60, 10);
 
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
-			}, 1000);
 
-			return minutes + ':' + seconds;
-  }
+        display.textContent = minutes + " : " + seconds;
 
-
-
-  this.isOn = false; //start with setting interveral to stop
-
-  this.start = function() {
-  	if (!this.isOn) {
-	    interval = setInterval(update.bind(this), 10); //updates every 10 milliseconds
-    	this.isOn = true;
-    }
-  };
-
-  this.stop = function() {
-  	if(this.isOn) {
-    	clearInterval(interval);
-      interval = null;
-      this.isOn = false;
-    }
-  };
-
-	this.increment = function() {
-		if(!this.isOn) {
-			time += delta();
-			var incrementTime = new Date(time); // we can now get a bunch of date methods
-			var minutes = (time.getMinutes().toString()) + 1000;
-			elem.textContent = incrementTime;
-			console.log(minutes);
-		}
-	};
-
-  this.reset = function() {
-		this.stop();
-		time = 0;
-		update();
-	};
+        if (--t< 0) {
+            t = duration;
+        }
+    }, 1000);
 }
+
+toggleBtn.addEventListener('click', function() {
+    var fiveMinutes = 60 * 5,
+        display = document.getElementById('timer');
+    startTimer(fiveMinutes, display);
+});
