253bc8bdc0294a68a8e36fe5408e07a959a706da
Alan Yeh <alan_yeh@apple.com>
2016-09-22 17:13:41 -0700

    added minus and add button

--- js/script.js
+++ js/script.js
@@ -1,16 +1,47 @@
-var timer = document.getElementById('timer');
+var display = document.getElementById('timer');
+var min = 5;
+var time = 60 * min;
 var toggleBtn = document.getElementById('toggle');
 var resetBtn = document.getElementById('reset');
 var plusBtn = document.getElementById('add');
+var minusBtn = document.getElementById('minus');
+
+var isOn = false;
+
+plusBtn.addEventListener('click', function() {
+	if (isOn == false) {
+		min++;
+		time = 60 * min;
+		min = min < 10 ? "0" + min : min; //add zero before number if less than 2
+		updateDisplay();
+	}
+});
+
+minusBtn.addEventListener('click', function() {
+	if (min > 0) {
+		if (isOn == false) {
+			min--;
+			time = 60 * min;
+			min = min < 10 ? "0" + min : min; //add zero before number if less than 2
+			updateDisplay();
+		}
+	}
+});
+
+// resetBtn.addEventListener('click', function() {
+// });
+//
+// pauseBtn.addEventListener('click', function() {
+// });
 
 function startTimer(duration, display) {
-    var t = duration, minutes, seconds;
+    var t = duration;
     setInterval(function () {
-        minutes = parseInt(t / 60, 10)
-        seconds = parseInt(t % 60, 10);
+        minutes = parseInt(t / 60)
+        seconds = parseInt(t % 60);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes; //add zero before number if less than 2
+        seconds = seconds < 10 ? "0" + seconds : seconds; //add zero before if less than 2
 
         display.textContent = minutes + " : " + seconds;
 
@@ -20,8 +51,11 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
+function updateDisplay() {
+	display.textContent = min + " : 00";
+};
+
 toggleBtn.addEventListener('click', function() {
-    var fiveMinutes = 60 * 5,
-        display = document.getElementById('timer');
-    startTimer(fiveMinutes, display);
+    startTimer(time, display);
+		isOn = true;
 });
