443b2970d5215bfb957758f478fed1845d819d93
Alan Yeh <alnyeh@gmail.com>
2016-10-01 17:29:15 -0700

    progress on pause functionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var display = document.getElementById('timer');
-var min = 5;
+var min = 30;
 var time = 60 * min;
 var toggleBtn = document.getElementById('toggle');
 var resetBtn = document.getElementById('reset');
 var plusBtn = document.getElementById('add');
 var minusBtn = document.getElementById('minus');
 var timerText = document.getElementById('timerText');
-
 var isOn = false;
+
 
 plusBtn.addEventListener('click', function() {
 	if (isOn == false) {
@@ -36,25 +36,26 @@ minusBtn.addEventListener('click', function() {
 // });
 
 function startTimer(duration, display) {
-    var t = duration;
-		if (!not)
-    setInterval(function () {
-        minutes = parseInt(t / 60)
-        seconds = parseInt(t % 60);
+		var t = duration;
+		var test = setInterval(function () {
+				minutes = parseInt(t / 60)
+				seconds = parseInt(t % 60);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes; //add zero before number if less than 2
-        seconds = seconds < 10 ? "0" + seconds : seconds; //add zero before if less than 2
+				minutes = minutes < 10 ? "0" + minutes : minutes; //add zero before number if less than 2
+				seconds = seconds < 10 ? "0" + seconds : seconds; //add zero before if less than 2
 
-        display.textContent = minutes + " : " + seconds;
+				display.textContent = minutes + " : " + seconds;
 
-        if (--t< 0) {
-            t = duration;
-        }
-    }, 1000);
+				if (--t< 0) {
+						t = duration;
+				}
+		}, 1000);
+		isOn = true;
+	}
 
-		function pause() {
-			clearInterval();
-		}
+	if (isOn = true) {
+		clearInterval(test);
+	}
 }
 
 
@@ -63,12 +64,12 @@ function updateDisplay() {
 };
 
 toggleBtn.addEventListener('click', function() {
+	var toggleColor = document.getElementById('timerDot');
 		if (!isOn) {
-    	startTimer(time, display);
-			isOn = true;
-			toggleBtn.textContent = 'Stop';
+			startTimer(time, display);
+			toggleColor.style.backgroundColor = '#F84982';
 		}
 		else {
-			toggleBtn.textContent = 'Start';
+			toggleColor.style.backgroundColor = '#189cc4';
 		}
 });
