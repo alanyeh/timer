2cf142a15e6b5ab790661f2770177c4db2049576
Alan Yeh <alnyeh@gmail.com>
2016-10-17 00:01:55 -0700

    adding resume and reset functionality

--- js/script.js
+++ js/script.js
@@ -6,12 +6,14 @@ var resetBtn = document.getElementById('reset');
 var plusBtn = document.getElementById('add');
 var minusBtn = document.getElementById('minus');
 var timerText = document.getElementById('timerText');
-var isOn = false;
+var state = 0; //  0 = idle, 1 = running, 2 = paused, 3= resumed
 var timerInterval;
+var resumeTime;
+var toggleColor = document.getElementById('timerDot');
 
 
 plusBtn.addEventListener('click', function() {
-	if (isOn == false) {
+	if (state == 0) {
 		min++;
 		time = 60 * min;
 		min = min < 10 ? "0" + min : min; //add zero before number if less than 2
@@ -21,7 +23,7 @@ plusBtn.addEventListener('click', function() {
 
 minusBtn.addEventListener('click', function() {
 	if (min > 0) {
-		if (isOn == false) {
+		if (state == 0) {
 			min--;
 			time = 60 * min;
 			min = min < 10 ? "0" + min : min; //add zero before number if less than 2
@@ -38,7 +40,7 @@ minusBtn.addEventListener('click', function() {
 
 function startTimer(duration, display) {
 		var t = duration;
-		if (isOn == false) {
+		if (state != 1) {
 			timerInterval = setInterval(function () {
 					minutes = parseInt(t / 60)
 					seconds = parseInt(t % 60);
@@ -47,12 +49,14 @@ function startTimer(duration, display) {
 					seconds = seconds < 10 ? "0" + seconds : seconds; //add zero before if less than 2
 
 					display.textContent = minutes + " : " + seconds;
+					resumeTime = ((minutes * 60) + (seconds % 60));
 
 					if (--t< 0) {
 							t = duration;
 					}
 			}, 1000);
-			isOn = true;
+			state = 1;
+			return resumeTime;
 		}
 }
 
@@ -62,13 +66,33 @@ function updateDisplay() {
 };
 
 toggleBtn.addEventListener('click', function() {
-	var toggleColor = document.getElementById('timerDot');
-		if (isOn == false) {
+		if (state == 0) {
 			startTimer(time, display);
 			toggleColor.style.backgroundColor = '#F84982';
 		}
-		else if(isOn == true) {
+		else if(state == 1) {
 			clearInterval(timerInterval);
+			state = 2;
 			toggleColor.style.backgroundColor = '#189cc4';
 		}
+		else if(state == 2) {
+			startTimer(resumeTime, display);
+			state = 3;
+			toggleColor.style.backgroundColor = '#F84982';
+		}
+});
+
+toggleBtn.addEventListener('dblclick', function(){
+	clearInterval(timerInterval);
+	time = 60 * min;
+	minutes = parseInt(time / 60)
+	seconds = parseInt(time % 60);
+
+	minutes = minutes < 10 ? "0" + minutes : minutes; //add zero before number if less than 2
+	seconds = seconds < 10 ? "0" + seconds : seconds; //add zero before if less than 2
+
+	display.textContent = minutes + " : " + seconds;
+
+	state = 0;
+	toggleColor.style.backgroundColor = '#F84982';
 });
